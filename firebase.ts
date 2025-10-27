972a946a9994a2f4452647e72482f9cfd26f1b5e
Yash Garg <22bcs13420@cuchd.in>
2025-10-28 01:43:39 +0530

    Fix .env leaks

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdr-0jB9tR-zrUF0RRSBgU9T05K1omf2k",
-  authDomain: "notion-clone-4203d.firebaseapp.com",
-  projectId: "notion-clone-4203d",
-  storageBucket: "notion-clone-4203d.firebasestorage.app",
-  messagingSenderId: "731889030417",
-  appId: "1:731889030417:web:47aaa9c4c3ebde625a68b0",
-  measurementId: "G-CVBQ8RMCK0"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
